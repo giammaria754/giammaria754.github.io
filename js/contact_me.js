2afac396a3e48b426275c88c71b6a1a365eb8dab
Giammaria Coscia <giammariauri@gmail.com>
2020-11-03 22:00:48 -0400

    Eliminato "vostro"

--- js/contact_me.js
+++ js/contact_me.js
@@ -32,7 +32,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Il vostro messaggio è stato inviato.</strong>");
+                        .append("<strong>Il messaggio è stato inviato.</strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
